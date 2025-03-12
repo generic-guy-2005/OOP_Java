83c008f58179a8740ab4fbe6d97bc32350ca0012
generic-guy-2005 <jevirahmat0@gmail.com>
2025-03-13 00:19:59 +0700

    Updated Program

--- Matrix.java
+++ Matrix.java
@@ -127,6 +127,32 @@ public class Matrix {
         return 1;
     }
     
+    public static void displayMatrix(int[][] matrix1, int[][] matrix2, int length) {
+        for(int i = 0; i < length; i++) {
+            for(int j = 0; j < length; j++) {
+                System.out.print(matrix1[i][j] + "\t");
+            }
+            System.out.print("\n");
+        }
+        System.out.println();
+        
+        for(int i = 0; i < length; i++) {
+            for(int j = 0; j < length; j++) {
+                System.out.print(matrix2[i][j] + "\t");
+            }
+            System.out.print("\n");
+        }
+    }
+    
+    public static int[][] generateMatrix(int[][] matrix, int length, Random dice) {
+        for(int i = 0; i < length; i++) {
+            for(int j = 0; j < length; j++) {
+                matrix[i][j] = dice.nextInt(10);
+            }
+        }
+        return matrix;
+    }
+    
     public static int menu(Scanner read) {
         System.out.println("+------------------+\n       MENU");
         System.out.println("1. Addition\n2. Subtraction\n3. Multiplication\n4. Identity\n5. Transpose\n6. Exit");
@@ -151,48 +177,40 @@ public class Matrix {
         matrix2 = new int[length][length];
         result = new int[length][length];
         
-        System.out.println("\nMatrix generated:");
-        for(int i = 0; i < length; i++) {
-            for(int j = 0; j < length; j++) {
-                matrix1[i][j] = dice.nextInt(10);
-            }
-        }
-        
-        for(int i = 0; i < length; i++) {
-            for(int j = 0; j < length; j++) {
-                matrix2[i][j] = dice.nextInt(10);
-            }
-        }
-        
-        for(int i = 0; i < length; i++) {
-            for(int j = 0; j < length; j++) {
-                System.out.print(matrix1[i][j] + "\t");
-            }
-            System.out.print("\n");
-        }
-        System.out.println();
-        
-        for(int i = 0; i < length; i++) {
-            for(int j = 0; j < length; j++) {
-                System.out.print(matrix2[i][j] + "\t");
-            }
-            System.out.print("\n");
-        }
-        
         switch(opt) {
             case 1:
+                System.out.println("\nMatrix generated:");
+                matrix1 = generateMatrix(matrix1, length, dice);
+                matrix2 = generateMatrix(matrix2, length, dice);
+                displayMatrix(matrix1, matrix2, length);
                 addition(matrix1, matrix2, length, result);
                 break;
             case 2:
+                System.out.println("\nMatrix generated:");
+                matrix1 = generateMatrix(matrix1, length, dice);
+                matrix2 = generateMatrix(matrix2, length, dice);
+                displayMatrix(matrix1, matrix2, length);
                 subtraction(matrix1, matrix2, length, result);
                 break;
             case 3:
+                System.out.println("\nMatrix generated:");
+                matrix1 = generateMatrix(matrix1, length, dice);
+                matrix2 = generateMatrix(matrix2, length, dice);
+                displayMatrix(matrix1, matrix2, length);
                 multiplication(matrix1, matrix2, length, result);
                 break;
             case 4:
+                System.out.println("\nMatrix generated:");
+                matrix1 = generateMatrix(matrix1, length, dice);
+                matrix2 = generateMatrix(matrix2, length, dice);
+                displayMatrix(matrix1, matrix2, length);
                 identity(length, result);
                 break;
             case 5:
+                System.out.println("\nMatrix generated:");
+                matrix1 = generateMatrix(matrix1, length, dice);
+                matrix2 = generateMatrix(matrix2, length, dice);
+                displayMatrix(matrix1, matrix2, length);
                 transpose(matrix1, matrix2, length, result);
                 break;
             case 6:
